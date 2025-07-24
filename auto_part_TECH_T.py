fcbae37413f371f32fe41d271f09e2ebcb2290ce
greglu1976 <purpleshiftter@yandex.ru>
2025-07-24 14:03:59 +0300

    initial

--- auto_part_TECH_T.py
+++ auto_part_TECH_T.py
@@ -14,7 +14,7 @@ from lib._PARTS.TECH_T import part_TECH_T
 class PartOfTECH_T_GUI:
     def __init__(self, root):
         self.root = root
-        self.root.title("Тестирование Газовых и Технологических Защит М300-Т. вер.0 от 03.04.25)")
+        self.root.title("Тестирование Газовых и Технологических Защит М300-Т. вер.0 от 03.04.25, вер.1 от 24.07.25)")
         self.part = None
         self.polling_thread = None
         self.is_polling = False
@@ -42,9 +42,9 @@ class PartOfTECH_T_GUI:
             "SGF1_rbre1_tofflvlgc": tk.IntVar(value=0),
             "SGF2_rbre1_tofflvlgc": tk.IntVar(value=0),
             "SGF3_rbre1_tofflvlgc": tk.IntVar(value=0),
-            "SGF1_rblc1_tofflvlgc": tk.IntVar(value=0),
-            "SGF2_rblc1_tofflvlgc": tk.IntVar(value=0),
-            "SGF3_rblc1_tofflvlgc": tk.IntVar(value=0),
+            "SGF1_lvcbrblc1_tofflvlgc": tk.IntVar(value=0),
+            "SGF2_lvcbrblc1_tofflvlgc": tk.IntVar(value=0),
+            "SGF3_lvcbrblc1_tofflvlgc": tk.IntVar(value=0),
             "SGF1_lvalh": tk.IntVar(value=0),
             "SGF2_lvalh": tk.IntVar(value=0),
             "SGF3_lvalh": tk.IntVar(value=0),
@@ -261,9 +261,9 @@ class PartOfTECH_T_GUI:
             SGF1_rbre1_tofflvlgc=self.sgf_params["SGF1_rbre1_tofflvlgc"].get(),
             SGF2_rbre1_tofflvlgc=self.sgf_params["SGF2_rbre1_tofflvlgc"].get(),
             SGF3_rbre1_tofflvlgc=self.sgf_params["SGF3_rbre1_tofflvlgc"].get(),
-            SGF1_rblc1_tofflvlgc=self.sgf_params["SGF1_rblc1_tofflvlgc"].get(),
-            SGF2_rblc1_tofflvlgc=self.sgf_params["SGF2_rblc1_tofflvlgc"].get(),
-            SGF3_rblc1_tofflvlgc=self.sgf_params["SGF3_rblc1_tofflvlgc"].get(),
+            SGF1_rblc1_tofflvlgc=self.sgf_params["SGF1_lvcbrblc1_tofflvlgc"].get(),
+            SGF2_rblc1_tofflvlgc=self.sgf_params["SGF2_lvcbrblc1_tofflvlgc"].get(),
+            SGF3_rblc1_tofflvlgc=self.sgf_params["SGF3_lvcbrblc1_tofflvlgc"].get(),
             SGF1_t_lvalh=self.sgf_params["SGF1_lvalh"].get(),
             SGF2_t_lvalh=self.sgf_params["SGF2_lvalh"].get(),
             SGF3_t_lvalh=self.sgf_params["SGF3_lvalh"].get(),
