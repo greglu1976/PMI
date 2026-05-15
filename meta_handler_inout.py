8327d9a73512f5baec4c55a9efb93d52d2167ac2
greglu1976 <purpleshiftter@yandex.ru>
2026-05-15 15:41:01 +0300

    up

--- meta_handler_inout.py
+++ meta_handler_inout.py
@@ -12,7 +12,7 @@ config_handler = MainConfigHandler.from_json_file(meta_path)
 #t = config_handler.get_param_info("T_LVCBSUP_1_RCBF1_FuncEnabled")
 #print(t) lvrbvtr1
 
-pars = config_handler.find_parameters_starting_with("T2_LVTTOC_1_KschemeCT")
+pars = config_handler.find_parameters_starting_with("ARNT")
 
 for par in pars:
     #if par["group"] ==  "measurement":  #"setting": # and par["fullDescription"] == "Внешнее отключение от УРОВ НН":
